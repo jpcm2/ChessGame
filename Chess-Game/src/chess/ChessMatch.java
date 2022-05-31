81f23d7435eb5155d0df9c3e57f698e16831230a
Joao Medeiros <jpcm2@cin.ufpe.br>
2022-05-31 11:24:47 -0300

    Criando Peças

--- Chess-Game/src/chess/ChessMatch.java
+++ Chess-Game/src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Torre;
 
 public class ChessMatch {
 	
@@ -8,6 +11,7 @@ public class ChessMatch {
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
+		this.initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -18,5 +22,11 @@ public class ChessMatch {
 			}
 		}
 		return tab;
+	}
+	
+	private void initialSetup() {
+		board.placePiece(new Torre(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new King(board, Color.BLACK), new Position(5, 3));
+
 	}
 }
