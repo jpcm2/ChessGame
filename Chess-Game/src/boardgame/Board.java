807b5b0d05f54509ed666c9836b8ec0381aa2c47
Joao Medeiros <jpcm2@cin.ufpe.br>
2022-05-31 11:39:07 -0300

    Programação Defensiva

--- Chess-Game/src/boardgame/Board.java
+++ Chess-Game/src/boardgame/Board.java
@@ -8,6 +8,9 @@ public class Board {
 	public Board() {}
 
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Número de linhas ou Colunas insuficientes");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -17,28 +20,54 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
-
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
 	
 	public Piece piece(int row, int column) {
+		if(row < 0 || row >= this.rows) {
+			throw new BoardException("Linha do tabuleiro inválida");
+		}
+		if(column < 0 || column >= this.columns) {
+			throw new BoardException("Coluna do tabuleiro inválida");
+		}
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
+		if(!this.positionExists(position)) {
+			throw new BoardException("Posição inválida");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
+		if(this.thereIsAPiece(position)) {
+			throw new BoardException("Já existe uma peça nessa posição");
+		}
 		piece.position = position;
 		pieces[position.getRow()][position.getColumn()] = piece;
+	}
+	
+	private boolean positionExists(int row, int column) {
+		if(row >= 0 && row < this.rows && column >= 0 && column < this.columns) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if(!this.positionExists(position)) {
+			throw new BoardException("Posição inválida");
+		}
+		if(piece(position.getRow(), position.getColumn()) == null) {
+			return false;
+		}else {
+			return true;
+		}
 	}
 }
